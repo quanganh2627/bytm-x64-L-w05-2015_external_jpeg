5121e250ba5655e324e7d3ad2d3bfbaa6eb46bdd
Rao, Chetan <chetan.rao@intel.com>
2013-12-02 02:04:36 -0800

    Revert "jpeg: TARGET_ARCH_VARIANT supports x86-slm"
    BZ: 155792
    
    This reverts commit d7f6ecbaf5325ffdbc3be5a44b558c65223fb2d6.
    
    Change-Id: Ia62ee6c2262c299485e45bb2d3d77650088961f2
    Category: aosp improvement
    Domain: AOSP-Framework-Media
    Origin: internal
    Upstream-Candidate: yes
    Signed-off-by: Chetan Rao <chetan.rao@intel.com>

--- Android.mk
+++ Android.mk
@@ -39,7 +39,7 @@ LOCAL_CFLAGS += -O3 -fstrict-aliasing -fprefetch-loop-arrays
 # enable tile based decode
 LOCAL_CFLAGS += -DANDROID_TILE_BASED_DECODE
 
-ifneq (, $(findstring x86-, $(TARGET_ARCH_VARIANT)))
+ifeq ($(TARGET_ARCH),x86)
   LOCAL_CFLAGS += -DANDROID_INTELSSE2_IDCT
   LOCAL_SRC_FILES += jidctintelsse.c
 endif
